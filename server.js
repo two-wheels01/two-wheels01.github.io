824522931efe69182750dfafb66fea67862f8478
two-wheels01 <mail.name@mailbox.org>
2021-08-26 16:38:18 +0100

    fix button hover issue

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 
 // instantiate an express app
 const app = express();
-app.use(cors({ origin: 'https://two-wheels01.github.io' }));
+app.use(cors({ origin: '*' }));
 
-//make the contact page the the first page on the app
+//make the contact page  the first page on the app
 app.route('/').get(function (req, res) {
   res.sendFile(process.cwd() + '/index.html');
 });
